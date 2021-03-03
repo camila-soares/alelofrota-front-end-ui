17035f49878cdf7165682301fa85e66a905721d6
camilasoares <camilasoares1507@gmail.com>
2021-03-03 14:42:00 -0300

    criando pipeline

--- src/app/incluir-veiculo/incluir-veiculo.component.spec.ts
+++ src/app/incluir-veiculo/incluir-veiculo.component.spec.ts
@@ -18,8 +18,4 @@ describe('IncluirVeiculoComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
